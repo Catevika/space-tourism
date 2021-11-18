673871c00ec0383a42f3749210ec476a5c771b12
Catevika <dominique.bello@outlook.fr>
2021-11-18 11:42:27 +0100

    Add Destination page

--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -1,5 +1,69 @@
+import Moon from "../assets/destination/image-moon.png";
+
 const Destination = () => {
-	return <h1>Destination</h1>;
+	return (
+		<main className='container flex flex-container flex-column'>
+			<h1 className='numbered-title'>
+				<span arai-label='hidden'>01</span> Pick your destination
+			</h1>
+			<div className='flex flex-container flex-center'>
+				<img src={Moon} alt='Moon planet' className='img-moon' />
+				<article className='destination-info'>
+					<div className='underline-indicators flex'>
+						{/* eslint-disable-next-line */}
+						<button
+							aria-selected='true'
+							className='uppercase bg-transparent ff-sans-cond text-white letter-spacing-2'
+						>
+							Moon
+						</button>
+						{/* eslint-disable-next-line */}
+						<button
+							aria-selected='false'
+							className='uppercase bg-transparent ff-sans-cond text-accent letter-spacing-2'
+						>
+							Mars
+						</button>
+						{/* eslint-disable-next-line */}
+						<button
+							aria-selected='false'
+							className='uppercase bg-transparent ff-sans-cond text-accent letter-spacing-2'
+						>
+							Europa
+						</button>
+						{/* eslint-disable-next-line */}
+						<button
+							aria-selected='false'
+							className='uppercase bg-transparent ff-sans-cond text-accent letter-spacing-1'
+						>
+							Titan
+						</button>
+					</div>
+					<h2 className='uppercase ff-serif text-light'>Moon</h2>
+					<p className='text-accent p-text'>
+						See our planet as you’ve never seen it before. A perfect relaxing
+						trip away to help regain perspective and come back refreshed. While
+						you’re there, take in some history by visiting the Luna 2 and Apollo
+						11 landing sites.
+					</p>
+					<div className='destination-meta flex flex-destination'>
+						<div>
+							<h3 className='uppercase text-accent'>
+								Avg. distance{" "}
+								<p className='uppercase ff-serif text-white'>384,400 km</p>
+							</h3>
+						</div>
+						<div>
+							<h3 className='uppercase text-accent'>
+								Est. travel time{" "}
+								<p className='uppercase ff-serif text-white'>3 days</p>
+							</h3>
+						</div>
+					</div>
+				</article>
+			</div>
+		</main>
+	);
 };
 
 export default Destination;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 const Home = () => {
 	return (
-		<main className='container flex flex-home'>
+		<main className='container flex flex-container flex-home'>
 			<div>
 				<h1 className='uppercase ff-sans-cond text-accent letter-spacing-1'>
 					So, you want to travel to
@@ -9,7 +9,7 @@ const Home = () => {
 						Space
 					</span>
 				</h1>
-				<p className='text-accent'>
+				<p className='text-accent p-text'>
 					Let’s face it; if you want to go to space, you might as well genuinely
 					go to outer space and not hover kind of on the edge of it. Well sit
 					back, and relax because we’ll give you a truly out of this world
